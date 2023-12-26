023de0d683dcf1cc1741aba4a8bbc10c07d553ec
Yusuf YILDIZ <a.yusuf.yildiz@hotmail.com>
2023-12-27 02:03:10 +0300

    refactor: add some comments

--- src/hooks/use-debounce.ts
+++ src/hooks/use-debounce.ts
@@ -5,9 +5,11 @@ export function useDebounce(callback: (...args: unknown[]) => void) {
   const ref = useRef<(...args: unknown[]) => void>();
 
   useEffect(() => {
+    // we store the callback in a ref so we can use latest callback without triggering a re-render
     ref.current = callback;
   }, [callback]);
 
+  // this useMemo ensures that we only create the debounced function once, notice the empty dependency array
   const debouncedCallback = useMemo(() => {
     const func = () => {
       ref.current?.();
